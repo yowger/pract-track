5941a92abfe65528335f00410214a1d2b5a1c270
Ver0018 <rogerpantilyowger@gmail.com>
2025-08-05 04:25:21 +0800

    v1

--- src/features/dashboard/pages/dashboard.tsx
+++ src/features/dashboard/pages/dashboard.tsx
@@ -1,3 +1,16 @@
+import { ChartAreaInteractive } from "../components/charts/chart-area-interactive"
+import { SectionCards } from "@/features/dashboard/components/section-cards"
+
+// import data from "@/data/mock-data.json"
+
 export default function Dashboard() {
-    return <div>dashboard</div>
+    return (
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+            <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+            </div>
+            {/* <DataTable data={data} /> */}
+        </div>
+    )
 }
